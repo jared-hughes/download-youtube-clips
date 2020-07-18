d1750c0915135363be0dfa8f7a1f239b9807b446
Jared Hughes <jahughes241@gmail.com>
2020-07-18 01:11:19 -0700

    Fix handling for video IDs beginning with a `-`

--- download_clip.bash
+++ download_clip.bash
@@ -7,5 +7,5 @@
 # $5: format, as accepted by youtube-dl (default: best)
 # other args are passed directly to youtube-dl; eg, -r 40K
 fmt=${5:-best}
-url="$(youtube-dl --get-url "$1" -f $fmt ${@:5})"
+url="$(youtube-dl --get-url -f $fmt ${@:5} -- "$1")"
 ffmpeg -loglevel warning -hide_banner -ss $2 -i "$url" -c copy -t $3 "$4"
